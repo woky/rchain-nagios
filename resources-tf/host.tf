096dff02799df1fc6b9bcc289d6a59e29dd22cdd
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-03-26 15:21:50 +0100

    Add files

---
+++ resources-tf/host.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance" "nagios_host" {
+  name = "nagios"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-minimal-1810"
+      size = 60
+      type = "pd-standard"
+    }
+  }
+
+  tags = [ "nagios", "http" ]
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/google_compute_engine")}"
+  }
+
+  provisioner "file" {
+    source = "${var.rchain_sre_git_crypt_key_file}"
+    destination = "/root/rchain-sre-git-crypt-key"
+  }
+
+  provisioner "remote-exec" {
+    script = "../update-run-setup"
+  }
+}
---
+++ resources-tf/main.tf
@@ -0,0 +1,6 @@
+variable "rchain_sre_git_crypt_key_file" {}
+
+provider "google" {
+  project = "developer-222401"
+  zone    = "us-east1-c"
+}
